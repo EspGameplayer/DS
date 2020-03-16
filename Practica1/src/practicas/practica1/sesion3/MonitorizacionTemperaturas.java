c5cfe6054432025267347adce51153825b9de2ad
José Antonio <jadg1999@gmail.com>
2020-03-16 20:08:54 +0100

    Update practica1.sesion3

--- Practica1/src/practicas/practica1/sesion3/MonitorizacionTemperaturas.java
+++ Practica1/src/practicas/practica1/sesion3/MonitorizacionTemperaturas.java
@@ -1,30 +1,33 @@
 package practicas.practica1.sesion3;
 
 public class MonitorizacionTemperaturas {
-
-	public static void main(String[] args) {
-		// Crear Sujeto Observable, el cual tiene la propiedad temperatura
-		SujetoObservable sujetoObservable = new SujetoObservable();
-
-		// Crear los 3 observadores
-		BotonCambio botonCambio = new BotonCambio();
-		GraficaTemperatura graficaTemperatura = new GraficaTemperatura();
-		PantallaTemperatura pantallaTemperatura = new PantallaTemperatura();
-
-		// Llamar al main del observable
-		botonCambio.main(new String[0]);
-
-		// Añadir a sujetoObservanble los 2 observadores que mantienen comunicación PULL
-		// con él
-		sujetoObservable.addObserver(botonCambio);
-		sujetoObservable.addObserver(graficaTemperatura);
-
-		// Comunicación PUSH con el sujeto observable
-		sujetoObservable.notifyObservers(pantallaTemperatura);
-
-		// Heredando de Thread sería:
-		// sujetoObservable.start();
-		Thread t = new Thread(sujetoObservable);
-		t.start();
-	}
+    
+    public static void main(String[] args) {
+        // Crear Sujeto Observable, el cual tiene la propiedad temperatura
+        SujetoObservable sujetoObservable = new SujetoObservable();
+        
+        // Crear los 3 observadores
+        BotonCambio botonCambio = new BotonCambio();
+        GraficaTemperatura graficaTemperatura = new GraficaTemperatura();
+        PantallaTemperatura pantallaTemperatura = new PantallaTemperatura();
+        
+        // Añadir a sujetoObservanble los 2 observadores que mantienen comunicación PULL con él
+        sujetoObservable.addObserver(botonCambio);
+        sujetoObservable.addObserver(graficaTemperatura);
+        
+        // Llamar al main de los observables
+        BotonCambio.main(new String[0]);
+        GraficaTemperatura.main(new String[0]);
+        PantallaTemperatura.main(new String[0]);
+        
+        // Comunicación PUSH con el sujeto observable
+        // sujetoObservable.notifyObservers(pantallaTemperatura);
+        
+        // Heredando de Thread sería:
+        // sujetoObservable.start();
+        Thread tPantallaTemperatura = new Thread(pantallaTemperatura);
+        Thread t = new Thread(sujetoObservable);
+        tPantallaTemperatura.start();
+        t.start();
+    }
 }
--- Practica1/src/practicas/practica1/sesion3/SujetoObservable.java
+++ Practica1/src/practicas/practica1/sesion3/SujetoObservable.java
@@ -6,36 +6,39 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class SujetoObservable extends Observable implements Runnable {
-	private Double temperatura;
+    private Double temperatura;
+    
+    public SujetoObservable(){
+        this.temperatura = ThreadLocalRandom.current().nextDouble(-20, 50);
+    }
+    
+    
+    @Override
+    public void run(){
+        while(true){
+            System.out.println("Run");
+            cambiarTemperatura();
+            try {
+                Thread.sleep(1000);
+                System.out.println("Me desperté");
+            } catch (InterruptedException ex) {
+                Logger.getLogger(SujetoObservable.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public void cambiarTemperatura() {
+        this.setTemperatura(ThreadLocalRandom.current().nextDouble(-20, 50));
+    }
 
-	public SujetoObservable() {
-		this.temperatura = ThreadLocalRandom.current().nextDouble(-20, 50);
-		setChanged();
-		notifyObservers(temperatura);
-	}
+    public Double getTemperatura() {
+	return this.temperatura;
+    }
 
-	@Override
-	public void run() {
-		while (true) {
-			System.out.println("Run");
-			this.setTemperatura(ThreadLocalRandom.current().nextDouble(-20, 50));
-			try {
-				Thread.sleep(3000);
-				System.out.println("Me desperté");
-			} catch (InterruptedException ex) {
-				Logger.getLogger(SujetoObservable.class.getName()).log(Level.SEVERE, null, ex);
-			}
-		}
-	}
-
-	public Double getTemperatura() {
-		return this.temperatura;
-	}
-
-	public void setTemperatura(Double temperarura) {
-		this.temperatura = temperarura;
-		setChanged();
-		notifyObservers(temperarura);
-	}
+    public void setTemperatura(Double temperarura) {
+	this.temperatura = temperarura;
+        setChanged();
+        notifyObservers(temperarura);
+    }
 
 }
